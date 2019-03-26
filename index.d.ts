2f40b450f1c52b77b07a31103d98c60c6a78433a
Jake Boone <jakeboone02@gmail.com>
2019-03-26 09:29:31 -0700

    Added TypeScript type definitions

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for react-spinner 0.2.7
+// Project: https://github.com/chenglou/react-spinner/
+// Definitions by: Jake Boone <https://github.com/jakeboone02>
+
+import * as React from 'react';
+
+export default class Spinner extends React.Component {}
